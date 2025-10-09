b22808f24cc25393e1380a0055c9a124d03e6ecb
Tejasai <66053533+tejasai1@users.noreply.github.com>
2025-10-09 14:21:33 +0000

    Change

--- src/mongo/dump.bash
+++ src/mongo/dump.bash
@@ -4,5 +4,5 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${SCRIPT_DIR}"
 
 docker compose exec mongo mongodump --uri="mongodb://mongo:gonmo@localhost" --out /data/db/dump
-cp -R /tmp/mongo/dump "${1}"
+sudo cp -R /tmp/mongo/dump "${1}"
 sudo rm -rf /tmp/mongo/dump
